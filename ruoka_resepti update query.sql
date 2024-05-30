f5014aad021b6b861650fb7d5273c95353315773
1Ebron23 <168309840+1Ebron23@users.noreply.github.com>
2024-05-30 17:05:07 +0300

    Update ruoka_resepti update query.sql

--- ruoka_resepti update query.sql
+++ ruoka_resepti update query.sql
@@ -1,5 +1,7 @@
---UPDATE dbo.Ingredients
---SET unit = 'cups'
---WHERE ingredientId = 5;
+-- Update unit to 'cups' for the ingredient with ingredientId = 5
+UPDATE dbo.Ingredients
+SET unit = 'cups'
+WHERE ingredientId = 5;
 
+-- Retrieve all ingredients from the Ingredients table
 SELECT * FROM dbo.Ingredients;
