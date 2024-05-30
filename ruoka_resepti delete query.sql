f0b9c164d99cfdea2786960a366db2362d9405a9
1Ebron23 <168309840+1Ebron23@users.noreply.github.com>
2024-05-30 17:12:25 +0300

    Update ruoka_resepti delete query.sql

--- ruoka_resepti delete query.sql
+++ ruoka_resepti delete query.sql
@@ -1,4 +1,6 @@
---SELECT * FROM dbo.CookingHardware;
-
+-- Delete a specific hardware item from the CookingHardware table
 DELETE FROM dbo.CookingHardware
 WHERE hardwareId = 20;
+
+-- Retrieve all remaining hardware items from the CookingHardware table
+SELECT * FROM dbo.CookingHardware;
